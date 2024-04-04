444728115ac19303f4296ab1bb9b44477f2b27d9
Tengiz Sharafiev <tengiz.sharafiev@p2p.org>
2024-04-04 09:53:51 +0300

    add mini actor for decoupling stream and proposer

--- src/lib.rs
+++ src/lib.rs
@@ -34,4 +34,36 @@ pub mod shared {
 
 pub mod sanitized {
     tonic::include_proto!("sanitized");
+
+    use anyhow::anyhow;
+    use solana_sdk::{hash::Hash, message::SimpleAddressLoader, transaction::MessageHash};
+
+    impl TryFrom<SanitizedTransaction> for solana_sdk::transaction::SanitizedTransaction {
+        type Error = anyhow::Error;
+
+        fn try_from(value: SanitizedTransaction) -> Result<Self, Self::Error> {
+            let SanitizedTransaction {
+                versioned_transaction,
+                message_hash,
+                loaded_addresses,
+            } = value;
+
+            let versioned_transaction = bincode::deserialize(&versioned_transaction)
+                .map_err(|_| anyhow!("failed to deserialize versioned_transaction"))?;
+            let message_hash = Hash::from(
+                TryInto::<[u8; 32]>::try_into(message_hash.as_slice())
+                    .map_err(|_| anyhow!("failed to deserialize message_hash"))?,
+            );
+            let loaded_addresses = bincode::deserialize(&loaded_addresses)
+                .map_err(|_| anyhow!("failed to deserialize loaded_addresses"))?;
+
+            solana_sdk::transaction::SanitizedTransaction::try_create(
+                versioned_transaction,
+                MessageHash::Precomputed(message_hash),
+                None,
+                SimpleAddressLoader::Enabled(loaded_addresses),
+            )
+            .map_err(|_| anyhow!("failed to create SanitizedTransaction"))
+        }
+    }
 }
