f8d6da8eae90e2ea9ce6e7704658046b5d26e1f7
Tengiz Sharafiev <tengiz.sharafiev@p2p.org>
2024-04-05 21:51:59 +0300

    forward packets and bundles to leader

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,69 @@ pub mod block_engine {
 }
 
 pub mod bundle {
+    use prost_types::Timestamp;
+    use solana_sdk::transaction::VersionedTransaction;
+    use std::time::SystemTime;
     tonic::include_proto!("bundle");
+
+    impl From<Vec<VersionedTransaction>> for BundleUuid {
+        fn from(transactions: Vec<VersionedTransaction>) -> Self {
+            let uuid = crate::derive_bundle_id(&transactions);
+            Self {
+                bundle: Some(transactions.into()),
+                uuid,
+            }
+        }
+    }
+
+    impl From<Vec<VersionedTransaction>> for Bundle {
+        fn from(transactions: Vec<VersionedTransaction>) -> Self {
+            Self {
+                packets: transactions.into_iter().map(Into::into).collect(),
+                header: Some(super::shared::Header {
+                    ts: Some(Timestamp::from(SystemTime::now())),
+                }),
+            }
+        }
+    }
 }
 
 pub mod packet {
+    use anyhow::Context;
+    use solana_sdk::transaction::VersionedTransaction;
     tonic::include_proto!("packet");
+
+    impl TryFrom<solana_sdk::packet::Packet> for Packet {
+        type Error = anyhow::Error;
+
+        fn try_from(p: solana_sdk::packet::Packet) -> Result<Self, Self::Error> {
+            Ok(Packet {
+                data: p.data(..).context("discarded packet")?.to_vec(),
+                meta: Some(Meta {
+                    size: p.meta().size as u64,
+                    addr: p.meta().addr.to_string(),
+                    port: p.meta().port as u32,
+                    flags: Some(PacketFlags {
+                        discard: p.meta().discard(),
+                        forwarded: p.meta().forwarded(),
+                        repair: p.meta().repair(),
+                        simple_vote_tx: p.meta().is_simple_vote_tx(),
+                        tracer_packet: p.meta().is_tracer_packet(),
+                    }),
+                    sender_stake: 0,
+                }),
+            })
+        }
+    }
+
+    impl From<VersionedTransaction> for Packet {
+        fn from(value: VersionedTransaction) -> Self {
+            solana_sdk::packet::Packet::from_data(None, value)
+                .unwrap()
+                .try_into()
+                .unwrap()
+        }
+    }
 }
 
 pub mod relayer {
@@ -66,4 +124,13 @@ pub mod sanitized {
             .map_err(|_| anyhow!("failed to create SanitizedTransaction"))
         }
     }
+}
+
+pub fn derive_bundle_id(transactions: &[solana_sdk::transaction::VersionedTransaction]) -> String {
+    use digest::Digest;
+    use itertools::Itertools;
+
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(transactions.iter().map(|tx| tx.signatures[0]).join(","));
+    format!("{:x}", hasher.finalize())
 }
